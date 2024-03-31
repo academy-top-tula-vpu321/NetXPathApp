eee66b94948a1a1fa19847702f203f5d762e02e5
maxefimov <efimov.top.academy@gmail.com>
2024-03-31 11:47:01 +0300

    Добавьте файлы проекта.

---
+++ NetXPathApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml;
+
+string xmlFileName = @"D:\employees.xml";
+
+XmlDocument xmlDocument = new XmlDocument();
+xmlDocument.Load(xmlFileName);
+
+XmlElement? root = xmlDocument.DocumentElement;
+
+var nodeList = root.SelectNodes("//Employee[@position]");
+
+foreach(XmlNode e in nodeList)
+    Console.WriteLine(e.OuterXml);
+
